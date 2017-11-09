dd991e19c1191e8d65d4dcdbb8631986e8b090b6
JonatanGust <jongust@student.chalmers.se>
2017-11-09 18:10:02 +0100

    hej

--- Lab2/BlackJackDelA.hs
+++ Lab2/BlackJackDelA.hs
@@ -14,6 +14,8 @@ size hand2
 --Hand w/ val 13
 exh = (Add (Card (Numeric 2) Hearts) (Add (Card Jack Spades)
         (Add (Card Ace Spades) Empty) ))
+exh2 = (Add (Card Ace Hearts) (Add (Card Ace Spades)
+        (Add (Card Ace Spades) Empty) ))
 
 --Returns the empty hand
 empty :: Hand
@@ -22,19 +24,21 @@ empty = Empty
 --Returns the value of the Hand
 value :: Hand -> Integer
 value Empty = 0
-value hand | val <= 21 = val
-    where val = valueHand(hand)
-value hand | otherwise = valueHand(hand) - (10 * numberOfAces(hand))
+value hand
+        | val <= 21 = val
+        | otherwise = val - (10 * numberOfAces(hand))
+        where val = valueHand(hand)
 
 --Returns value of Hand where aces = 11
 valueHand :: Hand -> Integer
-valueHand (Add c h) = valueCard c + value h
+valueHand Empty = 0
+valueHand (Add c h) = valueCard c + valueHand h
 
 --Returns value of Rank
 valueRank :: Rank -> Integer
-valueRank Ace = 11
-valueRank r | r==Jack || r==Queen || r==King = 10
 valueRank (Numeric r) = r
+valueRank Ace = 11
+valueRank r = 10
 
 --Returns value of Card
 valueCard :: Card -> Integer
@@ -43,8 +47,8 @@ valueCard (Card r s) = valueRank r
 --Returns number of aces in Hand
 numberOfAces :: Hand -> Integer
 numberOfAces Empty = 0
-numberOfAces (Add (Card r s) hand) | r == Ace = 1 + numberOfAces hand
-numberOfAces (Add card hand) = numberOfAces hand
+numberOfAces (Add (Card Ace s) hand) = 1 + numberOfAces hand
+numberOfAces (Add c hand) = numberOfAces hand
 
 
 --Returns true if Hand value is over 21
@@ -53,8 +57,7 @@ gameOver hand = value hand > 21
 
 --Returns the winner of two Hands, in order Guest, Bank
 winner :: Hand -> Hand -> Player
-winner h1 h2 | gameOver h1 = Bank
-winner h1 h2 | value h1 > value h2 = Guest
+winner h1 h2 | (value h1 > value h2) && not(gameOver h1) = Guest
 winner h1 h2 | otherwise = Bank
 
 
