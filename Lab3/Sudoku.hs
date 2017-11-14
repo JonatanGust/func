45a80297faed8e8ba1481901881804070ce99ad4
Palmmro <palmro@student.chalmers.se>
2017-11-14 15:16:12 +0100

    Lab3 part A, B

--- Lab3/Sudoku.hs
+++ Lab3/Sudoku.hs
@@ -25,6 +25,23 @@ example =
     n = Nothing
     j = Just
 
+exampleFail :: Sudoku
+exampleFail =
+    Sudoku
+      [ [j 9,j 6,n  ,n  ,j 7,j 1,j 2,n  ,n  ]
+      , [n  ,j 5,n  ,n  ,n  ,n  ,j 1,j 8,n,j 0  ]
+      , [n  ,n  ,j 9,j 2,n  ,j 4,j 7,n  ,n  ]
+      , [n  ,n  ,n  ,n  ,j 1,j 3,n  ,j 2,j 8]
+      , [j 4,n  ,n  ,j 5,n  ,j 2,n  ,n  ,j 9]
+      , [j 2,j 7,n  ,j 4,j 6,n  ,n  ,n  ,n  ]
+      , [n  ,n  ,j 5,j 3,n  ,j 8,j 9,n  ,n  ]
+      , [n  ,j 8,j 3,n  ,n  ,n  ,n  ,j 6,n  ]
+      , [n  ,n  ,j 7,j 6,j 9,n  ,n  ,j 4,j 3]
+      ]
+  where
+    n = Nothing
+    j = Just
+
 -- * A1
 
 -- | allBlankSudoku is a sudoku with just blanks
@@ -41,36 +58,25 @@ isSudoku s = length (rows s) == 9 && isRowsOk s
 
 isRowsOk :: Sudoku -> Bool
 isRowsOk (Sudoku []) = True
-isRowsOk s = (length x == 9) && isElementsOk x && (isRowsOk (Sudoku xs))
+isRowsOk s = (length x == 9) && isElementsOk x (Nothing:(map Just [1..9]))
+                             && (isRowsOk (Sudoku xs))
     where (x:xs) = (rows s)
 
-isElementsOk :: [Maybe Int] -> Bool
-isElementsOk [] = True
-isElementsOk (x:xs) = x `elem` [ Just 1
-                               , Just 2
-                               , Just 3
-                               , Just 4
-                               , Just 5
-                               , Just 6
-                               , Just 7
-                               , Just 8
-                               , Just 9
-                               , Nothing
-                               ]
-                       && isElementsOk xs
+isElementsOk :: [Maybe Int] -> [Maybe Int] -> Bool
+isElementsOk [] _ = True
+isElementsOk (x:xs) a = x `elem`  a
+                       && isElementsOk xs a
 
---isRowLength :: [a] -> Bool
---isRowLength s = length s == 9
---
---isColumnLen :: Sudoku -> Bool
---isColumnLen x:xs =
+
 
 -- * A3
 
 -- | isFilled sud checks if sud is completely filled in,
 -- i.e. there are no blanks
+-- can check if no nothing instead
 isFilled :: Sudoku -> Bool
-isFilled = undefined
+isFilled s = isElementsOk x (map Just [1..9]) && isFilled(Sudoku xs)
+        where (x:xs) = (rows s)
 
 -------------------------------------------------------------------------
 
@@ -79,14 +85,35 @@ isFilled = undefined
 -- |b printSudoku sud prints a nice representation of the sudoku sud on
 -- the screen
 printSudoku :: Sudoku -> IO ()
-printSudoku = undefined
+printSudoku s = do putStr (getSudokuStr s)
+
+
+getSudokuStr :: Sudoku -> String
+getSudokuStr s = (getRowStr x) ++"\n"++ getSudokuStr(Sudoku xs)
+            where (x:xs) = (rows s)
+
+
+getRowStr :: [Maybe Int] -> String
+getRowStr [] = ""
+getRowStr ((Just n):xs) = show n ++ getRowStr xs
+getRowStr (x:xs) = "." ++ getRowStr xs
 
 -- * B2
 
 -- | readSudoku file reads from the file, and either delivers it, or stops
 -- if the file did not contain a sudoku
 readSudoku :: FilePath -> IO Sudoku
-readSudoku = undefined
+readSudoku path = do text <- readFile path
+
+createSudoku :: String -> [[Maybe Int]]
+createSudoku str = (take 9 str): createSudokuList [(drop 11)]
+
+createSudokuList :: String -> [String]
+createSudokuList "" = [""]
+createSudokuList [str] = (take 9 str): (take 1 createSudokuList (drop 11 str))
+
+--createIntList :: [String] -> [[Maybe Int]]
+--createIntList str =
 
 -------------------------------------------------------------------------
 
