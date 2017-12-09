6fae27a89290b015443732fb5691dda0b56742b1
Palmmro <palmro@student.chalmers.se>
2017-12-09 15:04:59 +0100

    add update function

--- Project/Othello.hs
+++ Project/Othello.hs
@@ -32,19 +32,18 @@ exampleO =
 
 
 
---(!!=) :: [a] -> (Int,a) -> [a]
---(!!=) [] _                                = []
---(!!=) xs (n,_) | n < 0 || length(xs) <= n = xs
---(!!=) (x:xs) (0,a)                        = a:xs
---(!!=) (x:xs) (n,a)                        = x:((!!=) xs ((n-1),a))
---
---
-----Updates the sudoku with the given int at the given pos, returns updated sud
---update :: Othello -> Pos -> Maybe Brick -> Othello
---update (Othello o) p mb = Othello (o !!=
---                        (fst p, ((o !! (fst p)) !!= ((snd p),mb))))
---
---
+(!!=) :: [a] -> (Int,a) -> [a]
+(!!=) [] _                                = []
+(!!=) xs (n,_) | n < 0 || length(xs) <= n = xs
+(!!=) (x:xs) (0,a)                        = a:xs
+(!!=) (x:xs) (n,a)                        = x:((!!=) xs ((n-1),a))
+
+
+--Updates the sudoku with the given int at the given pos, returns updated sud
+update :: Othello -> Pos -> Maybe Brick -> Othello
+update (Othello o) p mb = Othello (o !!=
+                        (fst p, ((o !! (fst p)) !!= ((snd p),mb))))
+
 
 
 
