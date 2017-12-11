8fa323c51143807f82dad6f7de3f5f98090ef4c3
Palmmro <palmro@student.chalmers.se>
2017-12-11 16:31:14 +0100

    merge

--- Project/Main.hs
+++ Project/Main.hs
@@ -1,217 +1,113 @@
-module Main where
--- |  Web Fudgets Intro
--- Examples to introduce Web Fudgets (for declarative web programming)
--- Functional Programming course 2017.
--- Thomas Hallgren
+module Main2 where
 
-{-
-This started as a skeleton, the definitions were filled in during the lecture.
--}
---------------------------------------------------------------------------------
-
-import WebFudgets
-import HasteExtras(addStyleLink)
+import Haste
 import Haste.Graphics.Canvas
 import Othello
 
 --data CanvasBox = F (Picture ()) (MouseEvent, MouseData)
 
 
+
+
+
+import Haste.DOM
+import Haste.Events
+
+import Data.IORef
+
+import Pages
+
 main = do addStyleLink "style.css"
           runF (h2F (textF "WebFudgets OUtro") >+ boxTable)
 
-example1 = update exampleO (1,1) (Just White)
+--------------------------------------------------------------------------------
+-- Pure code, the program logic
+-- We follow the philosophy of doing as much as possible in the pure code.
 
---examples = tableF 8 $ canvasList exampleO--ex1 >+ ex2 >+ ex3 >+ ex4 >+ex5 >+ ex6 >+ ex7 >+ ex8 >+ex9 >+ex10
+type Size = (Double,Double)
 
-boxTable = (tableF 8 (canvasList example1)) `withF` css
-                where css = [style "border-collapse" =: "collapse",
-                             style "border-spacing" =: "0px 0",
-                             style "line-height" =: "0px"]
+radius :: Double
+radius = 15
 
+type Ball  = [Point]
+type State = [Ball]
 
+bounce :: Size -> Point -> Int -> Ball
+bounce (w,h) (x,y) v
+   | v == 0 && y >= maxY = replicate 20 (x,y)
+   | y' > maxY           = bounce (w,h) (x,y) (2-v)
+   | otherwise           = (x,y) : bounce (w,h) (x,y') v'
+ where
+   maxY = h-radius
+   v'   = v + 1
+   y'   = y + fromIntegral v
 
-canvasList :: Othello -> (F (Picture ()) (MouseEvent, MouseData))
-canvasList (Othello [])                     = emptybox
-canvasList (Othello [[]])                   = emptybox
-canvasList (Othello ([]:xs))                = canvasList (Othello xs)
-canvasList (Othello o) | elem == Just Black = blackbox >+ (canvasList newO)
-                       | elem == Just White = whitebox >+ (canvasList newO)
-                       | otherwise          = greenbox >+ (canvasList newO)
-    where elem = head (head o)
-          newO = Othello $ (drop 1 (head o)) : (drop 1 o)
+-- We use Int to represent velocity, because we need to compare it to 0, which
+-- is generally a bad idea for floating-point numbers (due to rounding errors).
 
-
-
-emptybox = canvasF (0,0)
-
-boxW = 40
-
-
-blackbox = box "black"
-greenbox = box "green"
-whitebox = box "white"
-
-box :: String -> (F (Picture ()) (MouseEvent, MouseData))
-box s = canvasF (boxW,boxW) `withF` css
-            where css = [style "border" =: "1px solid black",
-                   style "background" =: s]
+step :: State -> State
+step bs = [ ps | _:ps <- bs ]
+  -- Two purposes:
+  --
+  --  * Drop the first point in each ball
+  --  * Filter out finished balls (empty lists)
 
 
-
---ex9 = shellF "Canvas example" $
---      (pF sidesF >+< pF angleF) =>= gatherF =>= mapF polygon =>=
---      canvasF (300,300) `withF` css
---  where
---    --sidesF = textF "Draw a polygon with " +< smallF numberF >+ textF " sides."
---
---    angleF = toggleButtonF "Spin" =>= mapF setTimer =>= timerF
---              =>= putF 0 (stateF angle 0)
---    setTimer spin = if spin then Just (Repeat 40) else Nothing
---    angle a _ = ((a+1) `mod` 360 ,[pi*real a/180])
---
---    css = [style "border" =: "1px solid black",
---           style "background" =: "white"]
---
---real x = fromIntegral x
 
 --------------------------------------------------------------------------------
--- * Hello world example
+-- Interactive code
+-- This part is only concerned with interaction and drawing, not with the logic
+-- determining the behavior of the balls.
 
---ex1 = shellF "Hello" $ textF "Hello world!"
---
---
-----------------------------------------------------------------------------------
----- * Illustrates parallel composition
---
---ex2 = shellF "Hello world with a button" $
---      textF "Hello" >+< buttonF "Click me"
---
---
-----------------------------------------------------------------------------------
----- * Illustrates serial composition and stateless application code
---
---ex3 = shellF "A program to test the factorial function version 1" $
---      numberF =>= mapF fac =>= showF
---
---fac 0 = 1
---fac n = n * fac (n-1)
---
---
-----------------------------------------------------------------------------------
----- * Illustrates layout and decoration
---
---ex4 = shellF "A program to test the factorial function version 2" $
---      pF (textF "n=" +< smallF numberF)
---      =>= mapF fac =>=
---      pF (textF "n! = " +< showF)
---
---smallF fudget = fudget `withF` [style "width" =: "5em"]
---
-----------------------------------------------------------------------------------
----- * Illustrates stateful application code
---
---ex5 = shellF "An Up Counter" $
---      smallF showF =<= stateF incr 0 =<= buttonF "Up"
---
---incr n BtnClick = (n+1,[n+1])
---
-----------------------------------------------------------------------------------
----- * Illustrates how to handle output from a parallel composition
---
---ex6 = shellF "An Up/Down Counter" $
---      smallF showF =<= stateF count 0 =<= (buttonF "Up" >+< buttonF "Down")
---  where
---    count n (Left BtnClick) = (n+1,[n+1])
---    count n (Right BtnClick) = (n-1,[n-1])
---
-----------------------------------------------------------------------------------
----- * Illustrates parallel composition of a list of things
---
---data CounterButton = Up | Down | Reset
---                     deriving (Eq,Show,Bounded,Enum)
---
---ex7 = shellF "An Up/Down/Reset Counter" $
---      smallF showF =<= stateF count 0 =<=
---      listF [(t,buttonF (show t)) | t <- [Up .. Reset]]
---  where
---    count n (Up,BtnClick) = (n+1,[n+1])
---    count n (Down,BtnClick) = (n-1,[n-1])
---    count n (Reset,BtnClick) = (0,[0])
---
-----------------------------------------------------------------------------------
----- * Illustrates feedback loops
---
---ex8 = shellF "A Loadable Up/Down Counter" $
---      loopLeftF (mapF Left =<= smallF readShowF =<= stateF count 0)
---      =<= (buttonF "Up" >+< buttonF "Down")
---  where
---    count n (Left new) = (new,[])
---    count n (Right (Left BtnClick)) = (n+1,[n+1])
---    count n (Right (Right BtnClick)) = (n-1,[n-1])
---
---
-----------------------------------------------------------------------------------
----- * A larger example (more of the same)
---
---data CalculatorButton
---     = Plus | Minus | Times | Div
---     | Enter | Clear | Digit Int
---     deriving Eq
---
---
---ex9 = shellF "A Simple Calculator" $
---      showF =<= stateF calc [0] =<= keyboardF
---  where
---    keyboardF = tableF 4 buttonsF
---    buttonsF = listF [d 7, d 8, d 9,op Div,
---                      d 4, d 5, d 6,op Times,
---                      d 1, d 2, d 3,op Minus,
---                      clr,d 0, ent,op Plus]
---
---    d n = (Digit n,buttonF (show n))
---    op o = (o,buttonF (opLabel o))
---    ent = op Enter
---    clr = op Clear
---
---    opLabel Plus  = "+"
---    opLabel Minus = "-"
---    opLabel Times = "×"
---    opLabel Div   = "÷"
---    opLabel Enter = "Ent"
---    opLabel Clear = "C"
---
---    calc (n:s)   (Digit d,_) = new (n*10+d) s
---    calc s       (Enter,_)   = (0:s,[head s])
---    calc s       (Clear,_)   = ([0],[0])
---    calc (y:x:s) (Plus,_)    = new (x+y) s
---    calc (y:x:s) (Minus,_)   = new (x-y) s
---    calc (y:x:s) (Times,_)   = new (x*y) s
---    calc (y:x:s) (Div,_)     = new (x `div` y) s
---    calc s       _           = (s,[head s])
---
---    new n s = (n:s,[n])
---
-----------------------------------------------------------------------------------
----- * Illustrates the use of a canvas for graphical output
---
---ex10 = shellF "Canvas example" $
---       pF (textF "Draw a polygon with " +< readShowF >+ textF " sides.")
---       =>= mapF polygon =>=
---       canvasF (300,300)
---
---polygon n = do color (RGB 255 255 128) (fill (path ps))
---               color (RGB 0 0 255)     (stroke (path ps))
---               text (20,20) "A simple canvas example"
---  where
---    ps = map corner [0..n]
---
---    c@(cx,cy) = (150,150) -- center
---    r = 100 -- radius of enclosing circle
---
---    corner i = (cx+r*cos a,cy+r*sin a)
---      where
---        r = 100
---        a = 2*pi*(real i/ real n)
---
---real x = fromIntegral x
+ballShape :: Ball -> Shape ()
+ballShape []      = return ()
+ballShape (pos:_) = circle pos radius
+
+drawBall :: Ball -> Picture ()
+drawBall ball = do
+    color (RGB 255 0 0) $ fill $ ballShape ball
+    stroke $ ballShape ball
+
+animate :: Canvas -> IORef State -> IO ()
+animate can state = do
+    balls <- readIORef state
+    writeIORef state $ step balls
+    render can $ mapM_ drawBall balls
+    setTimer (Once 20) $ animate can state
+    return ()
+
+canWidth, canHeight :: Num a => a
+canWidth  = 500
+canHeight = 500
+
+main = do
+    canvas <- mkCanvas canWidth canHeight
+    clear  <- mkButton "clear"
+    column documentBody [canvas,clear]
+
+    setStyle documentBody "backgroundColor" "lightblue"
+    setStyle documentBody "textAlign" "center"
+
+    Just can <- fromElem canvas
+
+    -- Use an IORef to communicate between the animation and the event handlers
+    state <- newIORef []
+
+    -- Start the animation
+    animate can state
+
+    -- Set an event handler for clicks in the canvas
+    canvas `onEvent` Click $ \mouse -> do
+      let (x,y) = mouseCoords mouse
+          pos   = (fromIntegral x, fromIntegral y)
+          ball  = bounce (canWidth,canHeight) pos 0
+      balls <- readIORef state
+      writeIORef state (ball:balls)
+
+    -- Set an event handler for the clear button
+    clear `onEvent` Click $ \_ -> writeIORef state []
+
+-- Note: The current version of Haste does not run the event handler
+-- concurrently with the animation, so there's no risk of a race between the
+-- different uses of `writeIORef`. If it was truly concurrent, then atomic
+-- operations would have to be used instead (see the Data.IORef documentation).
