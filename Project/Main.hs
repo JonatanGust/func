184e8e608a6bebf803048ae535f65cc9531f28c5
Palmmro <palmro@student.chalmers.se>
2017-12-12 20:45:39 +0100

    add comments and remove unused code

--- Project/Main.hs
+++ Project/Main.hs
@@ -1,68 +1,55 @@
 module Main where
 
-import Haste
-import Haste.Graphics.Canvas
 import Othello
-
+import Haste.Graphics.Canvas
 import Haste.DOM
 import Haste.Events
-
-
 import Haste.Prim
 import Haste.Foreign
 import Data.IORef
 
---import Pages
-
-
---renderAll :: Othello -> IO ()
 brickSize = 90
-
-brickSizeInt :: Int
-brickSizeInt = 90
-
-size = brickSize * 8
-
+fieldSize = brickSize * 8
 
 main = do
-          canvas <- (mkCanvas (size,size) "white")
-          txtCanvas <- (mkCanvas (size/8,size) "white")
-          restart <- (mkButton "Restart")
-          column documentBody [canvas,txtCanvas,restart]
-          Just can <- fromElem (canvas)
-          ot <- newIORef (emptyO, Black)
-          drawAll can emptyO
-          Just txtCan <- fromElem (txtCanvas)
-          renderText txtCan (emptyO,Black)
-          canvas `onEvent` Click $ \mouse -> do
-                         (oc,p) <- readIORef ot
-                         let (x,y)  = mouseCoords mouse
-                             cX = x `div` brickSizeInt
-                             cY = y `div` brickSizeInt
-                             (b,on) = tryPB oc (cX,cY) p
-                             np     = decidePlayer on p
-                         if b
-                            then
-                                do let (lo, lp) = helpPlayers on np
-                                   writeIORef ot (lo,lp)
-                                   mapM_ (renderOnTop can) $ renderAll lo
-                                   renderText txtCan (lo,lp)
-                            else return ()
-                         (oc,p) <- readIORef ot
-                         if null $ allLM oc
-                            then renderWinner on txtCan (getWinner oc)
-                            else return ()
+      canvas <- (mkCanvas (fieldSize,fieldSize) "white")
+      txtCanvas <- (mkCanvas (fieldSize/8,fieldSize) "white")
+      restartBtn <- (mkButton "Restart")
+      column documentBody [canvas,txtCanvas,restartBtn]
+      Just can <- fromElem (canvas)
+      ot <- newIORef (emptyO, Black)
+      drawAll can emptyO
+      Just txtCan <- fromElem (txtCanvas)
+      renderText txtCan (emptyO,Black)
+      canvas `onEvent` Click $ \mouse -> do
+                     (oc,p) <- readIORef ot
+                     let (x,y)  = mouseCoords mouse
+                         cX = x `div` (round brickSize)
+                         cY = y `div` (round brickSize)
+                         (b,on) = tryPB oc (cX,cY) p
+                         np     = decidePlayer on p
+                     if b
+                        then
+                            do let (lo, lp) = helpPlayers on np
+                               writeIORef ot (lo,lp)
+                               mapM_ (renderOnTop can) $ renderAll lo
+                               renderText txtCan (lo,lp)
+                        else return ()
+                     (oc,p) <- readIORef ot
+                     if null $ allLM oc
+                        then renderWinner on txtCan (getWinner oc)
+                        else return ()
 
-
-          restart `onEvent` Click $ \_ -> do
+      restartBtn `onEvent` Click $ \_ -> do
            clearChildren documentBody
            main
            return ()
 
-          where
-            renderAll o = map (renderSquare) $ (concat (rows o)) `zip` posList
-            drawAll can o = mapM (renderOnTop can) $ renderAll o
+      where
+        renderAll o = map (squarePicture) $ (concat (rows o)) `zip` posList
+        drawAll can o = mapM (renderOnTop can) $ renderAll o
 
+-- displays the scores and the winner as the player with the most points
 renderWinner :: Othello -> Canvas -> Maybe Brick -> IO ()
 renderWinner o txtCan b = do
        render txtCan ( scale (2,2)   (text (20,20) ("White score: "++(show (getPS o White)))))
@@ -72,15 +59,16 @@ renderWinner o txtCan b = do
                         else if b == (Just Black) then "Black won!"
                                 else "White won!"
 
+--displays the scores and who's turn it is
 renderText :: Canvas -> (Othello,Brick) -> IO ()
 renderText txtCan (o,b) = do
     render txtCan ( scale (2,2)   (text (20,20) ("White score: "++(show (getPS o White)))))
     renderOnTop txtCan ( scale (2,2)   (text (120,20) ("Black score: "++ (show (getPS o Black)))))
     renderOnTop txtCan ( scale (2,2)   (text (220,20) ("It's "++(show b)++" player's turn")))
 
-
-renderSquare :: (Maybe Brick, (Pos)) -> Picture ()
-renderSquare (b,(x,y)) | b == (Just Black) =
+--takes a brick and position and returns the equivalent picture to be drawn
+squarePicture :: (Maybe Brick, (Pos)) -> Picture ()
+squarePicture (b,(x,y)) | b == (Just Black) =
                 (drawCircle (doublex*brickSize) (doubley*brickSize) "black" )
                        | b == (Just White) =
                 (drawCircle (doublex*brickSize) (doubley*brickSize) "white" )
@@ -90,16 +78,16 @@ renderSquare (b,(x,y)) | b == (Just Black) =
                            doublex = fromIntegral x
                            doubley = fromIntegral y
 
-squareShape :: Double -> Double -> Shape ()
-squareShape x y = rect ((x, y)) (((x+brickSize), (y+brickSize)))
-
-boardShape :: Double -> Double -> Shape ()
-boardShape x y = rect ((x, y)) (x+(brickSize*8), (y+(brickSize*8)))
-
+--a circleShape with diameter a little smaller than brickSize
 circleShape :: Double -> Double -> Shape ()
 circleShape x y = circle (x+(brickSize/2),y+(brickSize/2))
     ((brickSize - (brickSize / 10)) /2)
 
+--a squareShape with width and height brickSize
+squareShape :: Double -> Double -> Shape ()
+squareShape x y = rect ((x, y)) (((x+brickSize), (y+brickSize)))
+
+--draws a circleShape with border
 drawCircle :: Double -> Double -> String -> Picture ()
 drawCircle x y clr = do
      color (getRGB "green") $ fill $ squareShape x y
@@ -107,24 +95,23 @@ drawCircle x y clr = do
      color (getRGB clr) $ fill $ circleShape x y
      stroke $ circleShape x y
 
-drawBoard :: Double -> Double -> String -> Picture ()
-drawBoard x y clr = do
-    color (getRGB clr) $ fill $ squareShape x y
-    stroke $ squareShape x y
-
+--draws a squareShape with border
 drawSquare :: Double -> Double -> String -> Picture ()
 drawSquare x y clr = do
     color (getRGB clr) $ fill $ squareShape x y
     stroke $ squareShape x y
 
+--gets RGB from a color string
 getRGB clr | clr == "green" = (RGB 0   255 0)
            | clr == "black" = (RGB 0   0   0)
            | clr == "white" = (RGB 255 255 255)
 
 
+----------Methods copied from Pages.hs---------------------------------------
+---http://www.cse.chalmers.se/edu/year/2016/course/TDA555/Haste/Pages.hs-----
+-----------------------------------------------------------------------------
 
-----------Methods stolen from Pages.hs---------------------------------------
-
+-- `mkCanvas width height` makes a drawing canvas of the specified dimensions
 mkCanvas :: (Double,Double) -> String -> IO Elem
 mkCanvas (h,w) s =
     newElem "canvas" `with` [style "border"          =: "1px solid black",
