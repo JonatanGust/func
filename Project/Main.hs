a24f6840a5eebac5106df17f67cc12b7207bb512
Palmmro <palmro@student.chalmers.se>
2017-12-12 16:11:59 +0100

    listens to mouse clicks and add bounce example

--- Project/Main.hs
+++ Project/Main.hs
@@ -7,15 +7,94 @@ import Othello
 import Haste.DOM
 import Haste.Events
 
+
+import Haste.Prim
+import Haste.Foreign
 import Data.IORef
 
 import Pages
 
+type Square  = [Point]
+type State = [Square]
+
+
+
+--renderAll :: Othello -> IO ()
+
+brickSizeInt :: Int
+brickSizeInt = 100
+
+--brickSizeDouble :: Double
+--brickSizeDouble = fromIntegral brickSizeInt
+
+brickSize = 100
+o = emptyO
+p = Black
+
+decidePlayer :: Brick -> IO Brick
+decidePlayer Black = if canPM o White
+                        then return White
+                        else return Black
+decidePlayer White = if canPM o Black
+                        then return Black
+                        else return White
 main = do
-            canvas <- (mkCanvas "white")
-            column documentBody [canvas]
-            Just can <- fromElem (canvas)
-            render can (drawSquare 1 1 "black" )
+          canvas <- (mkCanvas "white")
+          column documentBody [canvas]
+          Just can <- fromElem (canvas)
+          render can (drawSquare 0 0 "black" )
+          renderOnTop can (drawSquare 100 100 "green" )
+          drawAll can o
+          --mapM (renderOnTop can) $ renderAll o
+          canvas `onEvent` Click $ \mouse -> do
+                         let (x,y)  = mouseCoords mouse
+                             pos    = (fromIntegral (x), fromIntegral (y))
+                             cX = x `div` brickSizeInt--(toInteger brickSize)
+                             cY = y `div` brickSizeInt--(toInteger brickSize)
+                             (b,on) = tryPB o (cX,cY) Black
+--                         renderOnTop can (drawSquare 100 100 "white" )
+--                         renderOnTop can (drawSquare 200 200 "black" )
+--                         drawAll can o
+                         --mapM_ (renderOnTop can) $ renderAll on
+
+
+                         if b
+                            then do o <- return on
+                                    p <- decidePlayer p
+                                    mapM_ (renderOnTop can) $ renderAll on
+                            else return ()
+
+
+
+
+--                         renderOnTop can (drawSquare (fst pos) (snd pos) "white" )
+
+
+            where
+          renderAll o = map (renderSquare) $ (concat (rows o)) `zip` posList
+          drawAll can o = mapM (renderOnTop can) $ renderAll o
+
+renderSquare :: (Maybe Brick, (Pos)) -> Picture ()
+renderSquare (b,(x,y)) | b == (Just Black) = (drawSquare (doublex*brickSize) (doubley*brickSize) "black" )
+                       | b == (Just White) = (drawSquare (doublex*brickSize) (doubley*brickSize) "white" )
+                       | otherwise         = (drawSquare (doublex*brickSize) (doubley*brickSize) "green" )
+                       where
+                           doublex = fromIntegral x
+                           doubley = fromIntegral y
+
+
+--step :: State -> State
+--step bs = [ ps | _:ps <- bs ]
+--
+--
+--animate :: Canvas -> IORef State -> IO ()
+--animate can state = do
+--    squares <- readIORef state
+----    writeIORef state $ step squares
+--    render can $ mapM_ drawSquare squares
+--    setTimer (Once 20) $ animate can state
+--    return ()
+--
 
 
 canvasOthello :: Othello -> Canvas
@@ -35,17 +114,15 @@ mkButton label =
                           attr "value" =: label]
 
 squareShape :: Double -> Double -> Shape ()
-squareShape x y = rect ((x, y)) (((x+20), (y+20)))
+squareShape x y = rect ((x, y)) (((x+brickSize), (y+brickSize)))
 
 
 drawSquare :: Double -> Double -> String -> Picture ()
 drawSquare x y clr = do
-    color (getRGB "white") $ fill $ squareShape x y
+    color (getRGB clr) $ fill $ squareShape x y
     stroke $ squareShape x y
 
 
-
---getRGB :: String -> (RGB x y z)
 getRGB clr | clr == "green" = (RGB 0   255 0)
            | clr == "black" = (RGB 0   0   0)
            | clr == "white" = (RGB 255 255 255)
@@ -53,6 +130,6 @@ getRGB clr | clr == "green" = (RGB 0   255 0)
 mkCanvas :: String -> IO Elem
 mkCanvas s =
     newElem "canvas" `with` [style "border"          =: "1px solid black",
-                             style "backgroundColor" =: "white",
-                             prop "width"            =: "400",--show width,
-                             prop "height"           =: "400"]--show height]
+                             style "backgroundColor" =: s,
+                             prop "width"            =: (show $8*brickSize),--show width,
+                             prop "height"           =: (show $8*brickSize)]--show height]
