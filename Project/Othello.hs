aa172104499a92ea112a8605f5c03b7f1d9d160f
JonatanGust <jongust@student.chalmers.se>
2017-12-12 20:45:31 +0100

    jag är klar nu palm

--- Project/Othello.hs
+++ Project/Othello.hs
@@ -54,38 +54,34 @@ palmO =
 -- the screen
 printO :: Othello -> IO ()
 printO o = do putStr (getOStr o)
--- Gets a string from a sudoku
+-- Gets a string representing the whole othello
 getOStr :: Othello -> String
 getOStr (Othello []) = ""
 getOStr o = (getRowStr x) ++"\n"++ getOStr(Othello xs)
             where (x:xs) = (rows o)
 
---Gets a string from a row in a sudoku
+--Gets a string from a row in an othello
 getRowStr :: [Maybe Brick] -> String
 getRowStr [] = ""
-getRowStr ((Just b):xs) = show b ++ getRowStr xs
+getRowStr ((Just b):xs) = text ++ getRowStr xs
+        where text = if b == Black then "B"
+                                    else "W"
 getRowStr (x:xs) = "." ++ getRowStr xs
 
---Maybe not needed...
---removeNothing, removes all Nothing elements from a list of Maybe _wildcard_
---removeN :: [Maybe a] -> [Maybe a]
---removeN []     = []
---removeN (x:xs) = if x == Nothing
---                    then removeN xs
---                    else x:(removeN xs)
-
---isOthelloOK
+--isOthelloOK checks that the othello is actually an othello (rows, columns)
 isOOK :: Othello -> Bool
 isOOK o = length(rows o) == 8 && isOROK o
---isOthelloRowsOK
+--isOthelloRowsOK checks that all rows in the given othello are ok
+--(can be less/more than 8)
 isOROK :: Othello -> Bool
 isOROK o = and $ map isROK oRows
     where oRows = rows o
---isRowOK
+--isRowOK checks that the length of the othello row is ok
 isROK :: [Maybe Brick] -> Bool
 isROK r = length r == 8
 
---Insert function
+--Insert function, inserts the given element at given position
+--(index 0) in list
 (!!=) :: [a] -> (Int,a) -> [a]
 (!!=) [] _                                = []
 (!!=) xs (n,_) | n < 0 || length(xs) <= n = xs
@@ -105,7 +101,8 @@ prop_insert a (n,mb)                              = (length a2 == length a)
                             lastA               = drop (n+1) a
                             lastA2              = drop (n+1) a2
 
---Updates the sudoku with the given int at the given pos, returns updated sud
+--Updates the othello with the given brick at the given pos,
+--returns updated othello
 update :: Othello -> Pos -> Maybe Brick -> Othello
 update (Othello o) p mb = Othello (o !!=
                         (fst p, ((o !! (fst p)) !!= ((snd p),mb))))
@@ -131,13 +128,14 @@ prop_update (Othello o) p mb          = (o2 !! (fst p')) !! (snd p') == mb
 --rules
 flipB :: Othello -> Pos -> Maybe Brick -> Othello
 flipB o p mb = flipB' o p mb (findBF o p mb)
-
+--Recursive helper function of flipBricks
 flipB' :: Othello -> Pos -> Maybe Brick -> [Pos] -> Othello
 flipB' o _ _ []    = o
 flipB' o p mb (x:xs) = flipB' o2 p mb xs
                     where o2 = flipBB o p mb x
 
 --flipBricksBetween
+--Flips bricks between the first and the second point
 --pA = point At, pG = point Goal, pC = point Cardinality, pN = point Next
 flipBB :: Othello -> Pos -> Maybe Brick -> Pos -> Othello
 flipBB o pA mb pG = if pA == pG
@@ -155,14 +153,13 @@ flipBB o pA mb pG = if pA == pG
                                         then 1
                                         else 0
                         pN = ((fst pA)+pCr,(snd pA)+pCc)
---List of directions
+--List of directions is a list of all directions as vectors (legal directions)
 --        (+,+),(+,0),(+,-) ,(0,-) ,(-,-)  ,(-,0) ,(-,+) ,(0,+)
 pCList = [(1,1),(1,0),(1,-1),(0,-1),(-1,-1),(-1,0),(-1,1),(0,1)]
 
 --findBricks(to)Flip finds all positions that bounds a flipping line
 findBF :: Othello -> Pos -> Maybe Brick -> [Pos]
 findBF o p mb = catMaybes(map (findBF' 1 o p mb) pCList)
-
 --findBricks(to)Flip' is a helper function that finds one positions that
 --bounds a flipping line given a cardinality (second pos)
 findBF' :: Int -> Othello -> Pos -> Maybe Brick -> Pos -> Maybe Pos
@@ -178,22 +175,19 @@ findBF' n o pA mb pC | (fst pN) < 0
                    | otherwise = findBF' (n+1) o pN mb pC
                    where pN = ((fst pA)+(fst pC),(snd pA)+(snd pC))
 
-
-
-
 --isLegalMove(for)Brick returns true if the given brick can be placed here
 isLMB :: Maybe Brick -> (Othello, Pos) -> Bool
 isLMB mb (o, p) = length (findBF o p mb) > 0
 
---allLegalMoves(for)Brick returns a list of all possible moves (pos) of the given
---brick type
+--allLegalMoves(for)Brick returns a list of all possible moves (pos) of the
+--given brick type
 allLMB :: Othello -> Maybe Brick -> [Pos]
 allLMB o mb = map snd $filter (isLMB mb) $zip (64 `replicate` o)
                 $filter (isPE o) posList
---isPosEmpty
+
+--isPosEmpty checks if the position is empty (Nothing)
 isPE :: Othello -> Pos -> Bool
 isPE o (r,c) = Nothing == ((rows o) !! r !! c)
-
 
 --allLegalMoves is a list of all positions where any brick can be placed (can
 --contain duplicates)
@@ -205,11 +199,14 @@ allLM o = (allLMB o (Just Black))++(allLMB o (Just White))
 placeBA :: Othello -> Pos -> Brick -> Othello
 placeBA o p b = flipB (update o p (Just b)) p (Just b)
 
---canPlayer*Brick*Move
+--canPlayer(Brick)Move returns true if there is atleast one move that the
+--player (brick) can do
 canPM :: Othello -> Brick -> Bool
 canPM o b = length (allLMB o (Just b)) > 0
 
---try(to)PlaceBrick
+--try(to)PlaceBrick places the brick at the given pos if it is a legal move,
+--returns a tuple of (success of op, result), wich is to say (true, updated
+--othello) or (false, given othello)
 tryPB :: Othello -> Pos -> Brick -> (Bool, Othello)
 tryPB o p b = if p `elem` (allLMB o (Just b))
                     then (True, placeBA o p b)
@@ -219,17 +216,22 @@ tryPB o p b = if p `elem` (allLMB o (Just b))
 getPS :: Othello -> Brick -> Int
 getPS o b = length $ filter ((Just b) == ) $ concat $ rows o
 
+--returns the winner of the current boardstate, nothing if draw
 getWinner :: Othello -> Maybe Brick
 getWinner o | (getPS o White) > (getPS o Black) = Just White
             | (getPS o White) < (getPS o Black) = Just Black
             | otherwise = Nothing
 
+--If there is only one avaible move for the given player, makes that move
+--and then tries again (updates active player also)
 helpPlayers :: Othello -> Brick -> (Othello, Brick)
-helpPlayers o p | 1 == length (allLMB o (Just p)) = helpPlayers no np
-                | otherwise                  = (o, p)
-            where no = snd $tryPB o (head (allLMB o (Just p))) p
-                  np = decidePlayer no p
+helpPlayers o p | canPM o p = (o, p)
+                | otherwise = helpPlayers no np
+                where    no = snd $tryPB o (head (allLMB o (Just p))) p
+                         np = decidePlayer no p
 
+--Given that a player has just made a move, this func decides who the next
+--player should be
 decidePlayer :: Othello -> Brick -> Brick
 decidePlayer o Black = if canPM o White
                         then White
