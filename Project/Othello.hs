5c297b58cd4e74ec0e054e63176e755e6338ed53
JonatanGust <jongust@student.chalmers.se>
2017-12-12 18:52:08 +0100

    hej palM

--- Project/Othello.hs
+++ Project/Othello.hs
@@ -41,9 +41,9 @@ palmO =
       , [w  ,w  ,w  ,w  ,w  ,w  ,w  ,w   ]
       , [w  ,w  ,w  ,w  ,w  ,w  ,w  ,w   ]
       , [w  ,w  ,w  ,w  ,w  ,w  ,w  ,w   ]
-      , [w  ,w  ,w  ,w  ,w  ,b  ,w  ,w   ]
-      , [w  ,w  ,b  ,w  ,b  ,w  ,w  ,w   ]
-      , [w  ,b  ,n  ,b  ,n  ,n  ,w  ,w   ]
+      , [w  ,w  ,w  ,w  ,w  ,w  ,w  ,w   ]
+      , [w  ,w  ,b  ,w  ,w  ,b  ,w  ,w   ]
+      , [w  ,b  ,n  ,b  ,w  ,w  ,b  ,w   ]
       ]
   where
     n = Nothing
@@ -188,7 +188,12 @@ isLMB mb (o, p) = length (findBF o p mb) > 0
 --allLegalMoves(for)Brick returns a list of all possible moves (pos) of the given
 --brick type
 allLMB :: Othello -> Maybe Brick -> [Pos]
-allLMB o mb = map snd $filter (isLMB mb) $zip (64 `replicate` o) posList
+allLMB o mb = map snd $filter (isLMB mb) $zip (64 `replicate` o)
+                $filter (isPE o) posList
+--isPosEmpty
+isPE :: Othello -> Pos -> Bool
+isPE o (r,c) = Nothing == ((rows o) !! r !! c)
+
 
 --allLegalMoves is a list of all positions where any brick can be placed (can
 --contain duplicates)
